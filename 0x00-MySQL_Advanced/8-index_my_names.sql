46e977590a5b4c9e40476503c3d7277d976887a4
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-06-14 01:04:13 +0000

    fix: drop index if exists

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,5 @@
 -- This SQL script creates an index idx_name_first on
 -- the table names and the first letter of name.
 
-ALTER TABLE names
-ADD first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED,
-ADD INDEX idx_name_first (first_letter);
+DROP INDEX IF EXISTS idx_name_first;
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
