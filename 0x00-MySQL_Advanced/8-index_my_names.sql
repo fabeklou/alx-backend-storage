2e7b3e019f832ceb0353bf53ba3a39ca214295c7
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-06-14 01:09:49 +0000

    fix: Alter the table

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,9 @@
 -- This SQL script creates an index idx_name_first on
 -- the table names and the first letter of name.
 
-DROP INDEX IF EXISTS idx_name_first;
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+-- Add a generated column for the first letter of name
+ALTER TABLE names
+ADD COLUMN name_first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
+
+-- Create an index on the generated column
+CREATE INDEX idx_name_first ON names (name_first_letter);
