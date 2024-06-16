2b9e07c0a634f6abf8e3de67550ac2a5cad7a67d
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-16 01:12:53 +0000

    feat: change all topics of a school document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function to update the topics field
+of a document in a MongoDB collection.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics field of a document in the given MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB
+            collection to update.
+        name (str): The name of the document to update.
+        topics (list): The new list of topics to set for the document.
+
+    Returns:
+        None
+    """
+    mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
