36a328dac7a888c9a8f9ae840bc86b49dd40be40
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-21 01:03:13 +0000

    test: add test for web module

---
+++ 0x02-redis_basic/test_web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from web import get_page
+import redis
+import time
+
+
+redis_client = redis.Redis()
+
+URL = 'http://google.com'
+# URL = 'http://slowwly.robertomurray.co.uk'
+
+
+# Generate cache key and count key
+cache_key = "cache:{}".format(URL)
+count_key = "count:{}".format(URL)
+
+get_page(URL)
+print(redis_client.get(count_key))
+print(redis_client.get(cache_key))
+
+get_page(URL)
+print(redis_client.get(count_key))
+print(redis_client.get(cache_key))
+
+time.sleep(10)
+
+print(redis_client.get(count_key))
+print(redis_client.get(cache_key))
+
+print(redis_client.get(count_key))
+print(redis_client.get(cache_key))
